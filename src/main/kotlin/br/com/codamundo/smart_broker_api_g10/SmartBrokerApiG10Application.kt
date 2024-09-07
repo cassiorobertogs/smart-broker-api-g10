8ee71ed1e64bb0464662e357f41861248560a7c9
cassiorobertogs <adv.cassioroberto@gmail.com>
2024-09-07 12:31:18 -0400

    initial commit

---
+++ src/main/kotlin/br/com/codamundo/smart_broker_api_g10/SmartBrokerApiG10Application.kt
@@ -0,0 +1,12 @@
+package br.com.codamundo.smart_broker_api_g10
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartBrokerApiG10Application
+
+fun main(args: Array<String>) {
+	runApplication<SmartBrokerApiG10Application>(*args)
+}
+I
